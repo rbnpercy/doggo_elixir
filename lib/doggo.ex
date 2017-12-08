2832637353e5b630d0148b1e1dff53d77825a50e
Robin Percy <robin.percy@auth0.com>
2017-12-08 00:56:34 +0000

    Adding functionality...

--- lib/doggo.ex
+++ lib/doggo.ex
@@ -7,6 +7,31 @@ defmodule Doggo do
 
   @api_url "https://dog.ceo/api"
 
+  @doc """
+    The base URL for the Doggo API.
+  """
+  def process_url(url) do
+    @api_url <> url
+  end
+
+  @doc """
+    Process the response body.
+  """
+  def process_response_body(body) do
+    case Poison.decode(body) do
+      { :ok, data } -> data["message"]
+      _ -> body
+    end
+  end
+
+
+  @doc """
+    Gets a random Doggo.
+  """
+  def random do
+    Doggo.get!("/breeds/image/random")
+  end
+
 
 
 end
